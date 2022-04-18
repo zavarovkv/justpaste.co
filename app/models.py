6868329fb27bc45b29a34af2d26e7664b1e35df8
Konstantin Zavarov <zavarovkv@gmail.com>
2022-04-18 20:44:31 +0300

    change models.py

--- app/models.py
+++ app/models.py
@@ -3,14 +3,20 @@ from app import db
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.current_timestamp())
     title = db.Column(db.String(128), nullable=False)
     content = db.Column(db.UnicodeText, nullable=False)
+
+    # Additional important attributes
+    created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.current_timestamp())
+    size = db.Column(db.Integer(0), nullable=False)
+
+    # And other attributes in JSON format
     attributes = db.Column(db.JSON, nullable=False)
 
-    def __init__(self, title, content, attributes):
+    def __init__(self, title, content, attributes, size):
         self.title = title
         self.content = content
+        self.size = size
         self.attributes = attributes
 
     def __repr__(self):
